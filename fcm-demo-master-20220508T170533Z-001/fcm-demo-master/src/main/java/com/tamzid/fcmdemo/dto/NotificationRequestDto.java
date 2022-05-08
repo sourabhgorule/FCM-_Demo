a773250471174101a84d1485846fe05ad616c01d
Sourabh Gorule <58423559+sourabhgorule@users.noreply.github.com>
2022-05-08 23:38:04 +0530

    Add files via upload

---
+++ fcm-demo-master-20220508T170533Z-001/fcm-demo-master/src/main/java/com/tamzid/fcmdemo/dto/NotificationRequestDto.java
@@ -0,0 +1,14 @@
+package com.tamzid.fcmdemo.dto;
+
+import lombok.Data;
+
+@Data
+public class NotificationRequestDto {
+
+    private String target;
+    private String title;
+    private String body;
+    
+    
+    
+}
